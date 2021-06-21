0d4348b215323377adc921c90eae9f19ba87f574
indrawirananta <indra21112000@gmail.com>
2021-06-21 11:48:44 +0800

    added finding optimum k value function

--- KNN.py
+++ KNN.py
@@ -1,6 +1,7 @@
 # Make Predictions with k-nearest neighbors on the Iris Flowers Dataset
 import pandas as pd
 from math import sqrt
+import random
 
 
 def getdata(filename, sheet):
@@ -69,32 +70,6 @@ for i in range(len(data)):
     else:
         data_latih.append(data[i])
 
-# datalatih = pd.DataFrame(data_latih)
-# datauji = pd.DataFrame(data_uji)
-
-# datalatih.to_excel('./output_latih.xlsx', index=False)
-# datauji.to_excel('./output_uji.xlsx', index=False)
-
-# print(len(data_latih))
-# print(len(data_uji))
-# print(f"jarak row 1 dan 2 : {euclidean_distance(data[0],data[1])}")
-# print(f"jarak row 1 dan 6 : {euclidean_distance(data[0],data[5])}")
-# print(f"jarak row 1 dan 10 : {euclidean_distance(data[0],data[9])}")
-# print(f"jarak row 1 dan 15 : {euclidean_distance(data[0],data[14])}")
-# print(f"jarak row 1 dan 20 : {euclidean_distance(data[0],data[19])}")
-# print(f"jarak row 1 dan 25 : {euclidean_distance(data[0],data[24])}")
-# print(f"jarak row 1 dan 30 : {euclidean_distance(data[0],data[29])}")
-# print(f"jarak row 1 dan 100 : {euclidean_distance(data[0],data[99])}")
-# print(f"jarak row 1 dan 110 : {euclidean_distance(data[0],data[109])}")
-# print(f"jarak row 1 dan 120 : {euclidean_distance(data[0],data[119])}")
-# print(f"jarak row 1 dan 130 : {euclidean_distance(data[0],data[129])}")
-# print(f"jarak row 1 dan 140 : {euclidean_distance(data[0],data[139])}")
-# print(f"jarak row 1 dan 150 : {euclidean_distance(data[0],data[149])}")
-# print(f"jarak row 1 dan 200 : {euclidean_distance(data[0],data[199])}")
-# print(f"jarak row 1 dan 300 : {euclidean_distance(data[0],data[299])}")
-# print(f"jarak row 1 dan 400 : {euclidean_distance(data[0],data[399])}")
-# print(f"jarak row 1 dan 500 : {euclidean_distance(data[0],data[499])}")
-
 
 # Locate the most similar neighbors
 def get_neighbors(train, test_row, num_neighbors):
@@ -117,13 +92,40 @@ def predict_classification(train, test_row, num_neighbors):
     return prediction
 
 
-num_neighbors = 3
+def classification_testing(train, test, test_iter, num_neighbors):
+    result = []
+    accuracy = 1
+    for i in range(test_iter):
+        n = random.randrange(1, 300)
+        label = predict_classification(train, test[n], num_neighbors)
+        label_actual = test[n][1]
+        result.append([label, label_actual])
+        if label == label_actual:
+            accuracy += 1
+    return accuracy / test_iter
 
-label = predict_classification(data_latih, data_uji[200], num_neighbors)
-print('Predicted: %s' % (label))
-label = predict_classification(data_latih, data_uji[250], num_neighbors)
-print('Predicted: %s' % (label))
-label = predict_classification(data_latih, data_uji[50], num_neighbors)
-print('Predicted: %s' % (label))
-label = predict_classification(data_latih, data_uji[150], num_neighbors)
-print('Predicted: %s' % (label))
+
+def optimal_k_value(train, test, test_iter, k_step, k_max):
+    max_acc = 0
+    optimal_k = 1
+    for i in range(1, k_max, k_step):
+        accuracy = classification_testing(train, test, test_iter, i)
+        if accuracy > max_acc:
+            max_acc = accuracy
+            optimal_k = i
+    return optimal_k, max_acc
+
+
+k_value, accuracy = optimal_k_value(data_latih, data_uji, 100, 1, 50)
+print("Best k value : ", k_value)
+print("Best accuracy : ", accuracy)
+
+# num_neighbors = 3
+# label = predict_classification(data_latih, data_uji[200], num_neighbors)
+# print('Predicted: %s' % (label))
+# label = predict_classification(data_latih, data_uji[250], num_neighbors)
+# print('Predicted: %s' % (label))
+# label = predict_classification(data_latih, data_uji[50], num_neighbors)
+# print('Predicted: %s' % (label))
+# label = predict_classification(data_latih, data_uji[150], num_neighbors)
+# print('Predicted: %s' % (label))
